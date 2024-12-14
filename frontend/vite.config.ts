51e8e73f0719f7ae5ab692cd575b7e3b0c95b868
Tarik Karaca <karaca.tarik@outlook.com>
2024-12-14 01:36:08 +0100

    feat: added React frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        outDir: "../build/frontend-resources/static/",
+    },
+    server: {
+        proxy: {
+            "/api": "http://localhost:8080",
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
---
+++ src/main/java/de/karaca/csrparser/SPAController.java
@@ -0,0 +1,12 @@
+package de.karaca.csrparser;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SPAController {
+    @GetMapping(value = "{path:^(?!api|public|assets)[^\\.]*}/**")
+    public String forward() {
+        return "forward:/";
+    }
+}
